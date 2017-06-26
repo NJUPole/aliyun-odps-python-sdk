95e1d55f656aa3d5df5093c233ecff649e9e130b
Heks <heks93@163.com>
2017-06-26 11:45:48 +0800

    Update build_wheels.sh

--- build_wheels.sh
+++ build_wheels.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 set -e -x
 
+git clone https://github.com/NJUPole/aliyun-odps-python-sdk /io/odps
+cd /io/aliyun-odps-python-sdk
 
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
-    "${PYBIN}/pip" install cython
-    "${PYBIN}/pip" install -r /io/requirements-full.txt
-    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+	if [[ ${PYBIN} != *"26"* ]] && [[ ${PYBIN} != *"33"* ]] && [[ ${PYBIN} != *"34"* ]]; then
+		"${PYBIN}/pip" install cython
+		"${PYBIN}/pip" install -r /io/odps/requirements-full.txt
+		"${PYBIN}/pip" wheel /io/odps -w wheelhouse/
 done
 
-# Install packages and test
-for PYBIN in /opt/python/*/bin/; do
-    "${PYBIN}/pip" install aliyun-odps-python-sdk --no-index -f /io/wheelhouse
+for whl in wheelhouse/pyodps*.whl; do
+    if [[ ${whl} == *"pyodps"* ]]; then
+        auditwheel repair $whl -w /io/wheelhouse/
+    fi
 done
+
