2f10009edcd04e9a957fc09ceb5e92551dec3eb3
Heks <heks93@163.com>
2017-06-27 16:06:24 +0800

    copy to root dir

--- multibuild/travis_linux_steps.sh
+++ multibuild/travis_linux_steps.sh
@@ -79,7 +79,6 @@ function build_multilinux {
     local build_cmds="$2"
     local docker_image=quay.io/pypa/manylinux1_$plat
     retry docker pull $docker_image
-    cp /multibuild .
     docker run --rm \
         -e BUILD_COMMANDS="$build_cmds" \
         -e PYTHON_VERSION="$MB_PYTHON_VERSION" \
@@ -92,7 +91,7 @@ function build_multilinux {
         -e PLAT="$PLAT" \
         -v $PWD:/io \
 		--privileged \
-        $docker_image /io/docker_build_wrap.sh
+        $docker_image /io/$MULTIBUILD_DIR/docker_build_wrap.sh
 }
 
 function install_run {
