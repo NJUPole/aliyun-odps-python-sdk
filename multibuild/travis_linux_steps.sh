b26858e4b37d4d35aa23437969364c29552d0125
Heks <heks93@163.com>
2017-06-27 15:28:10 +0800

    luan

--- multibuild/travis_linux_steps.sh
+++ multibuild/travis_linux_steps.sh
@@ -91,7 +91,7 @@ function build_multilinux {
         -e PLAT="$PLAT" \
         -v $PWD:/io \
 		--privileged \
-        $docker_image /io/docker_build_wrap.sh
+        $docker_image /io/multibuild/common_utils.sh
 }
 
 function install_run {
