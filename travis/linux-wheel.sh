a7063e8bbcf863ba165149801dfb18a401280490
Heks <heks93@163.com>
2017-06-27 19:54:09 +0800

    path config

--- travis/linux-wheel.sh
+++ travis/linux-wheel.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 set -e -x
-
-echo $(PYVER)
-
 PYBIN=/opt/python/${PYVER}/bin
 # Compile wheels
-${PYBIN}/pip install -r ../requirements-full.txt
+${PYBIN}/pip install -r /io/requirements-full.txt
 ${PYBIN}/pip install cython
 ${PYBIN}/pip wheel -w wheelhouse/ .
