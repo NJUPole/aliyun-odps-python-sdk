ff3d62bb40cdf2ce9c459a0c42b0ab2e95e30ade
Heks <heks93@163.com>
2017-06-27 22:41:50 +0800

    chmod

--- travis/linux-install.sh
+++ travis/linux-install.sh
@@ -1,5 +1,3 @@
 docker pull $DOCKER_IMAGE
 export PYVER=$PYVER
-sudo chmod 777 travis/linux-wheel.sh
-sudo chmod 777 travis/osx-wheel.sh
 docker run --rm -e "PYVER=$PYVER" -v $pwd:/io $DOCKER_IMAGE $PRE_CMD /io/travis/linux-wheel.sh
