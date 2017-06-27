b6fcc2ce4468a7a9e8ab969faae5f7f6cc056a40
Heks <heks93@163.com>
2017-06-27 21:34:09 +0800

    work out

--- travis/linux-wheel.sh
+++ travis/linux-wheel.sh
@@ -4,7 +4,6 @@ PYBIN=/opt/python/${PYVER}/bin
 ${PYBIN}/pip install --disable-pip-version-check --user --upgrade pip
 ${PYBIN}/pip install cython
 cd /io/
-mkdir wheelhouse
 # Compile wheels
 ${PYBIN}/python setup.py build
 ${PYBIN}/python setup.py bdist_wheel
