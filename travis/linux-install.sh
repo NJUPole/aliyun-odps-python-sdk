53a707073c0dbbf0fdee5f28b90a90a9b57ea76e
Heks <heks93@163.com>
2017-06-27 22:48:19 +0800

    docker -v fix

--- travis/linux-install.sh
+++ travis/linux-install.sh
@@ -1,3 +1,3 @@
 docker pull $DOCKER_IMAGE
 export PYVER=$PYVER
-docker run --rm -e "PYVER=$PYVER" -v $pwd:/io $DOCKER_IMAGE $PRE_CMD /io/travis/linux-wheel.sh
+docker run --rm -e "PYVER=$PYVER" -v `pwd`:/io $DOCKER_IMAGE /io/travis/linux-wheel.sh
