f943a8b418589a017364e11104138ba877d52efd
Heks <heks93@163.com>
2017-06-28 20:22:56 +0800

    test osx

--- bin/travis/osx-install.sh
+++ bin/travis/osx-install.sh
@@ -8,22 +8,23 @@ PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 pyenv install $PYTHON
 pyenv global $PYTHON
-python -V
+
 pip install --disable-pip-version-check --user --upgrade pip
 pip install cython
 pip install wheel
 # wheel
-python setup.py install
-python setup.py bdist_wheel
-#pip wheel --no-deps .
+#python setup.py bdist_wheel
+pip wheel --no-deps .
 #repair_wheel
-#mkdir dist
-#cp *.whl dist/
-#pip install delocate
-#delocate-wheel dist/*.whl
-#delocate-addplat --rm-orig -x 10_9 dist/*.whl
+mkdir dist
+cp *.whl dist/
+pip install delocate
+delocate-wheel dist/*.whl
+delocate-addplat --rm-orig -x 10_9 dist/*.whl
 ls dist
 cd dist
-pip install pyodps*.whl
+python -V
+pip uninstall pyodps
+pip install pyodps*.whl --no-cache
 python -c "from odps.tunnel.checksum_c import Checksum"
 python -c "print('success')"
